0c57a1fcfe26a3ab76ddaec65ff78d47eeb7c334
Miranda Livengood <miranda.livengood@sjsu.edu>
2023-09-20 01:57:47 +0000

    VERY simple membership page. To be formatted.

---
+++ movie-site/src/components/mem_options.js
@@ -0,0 +1,23 @@
+export default function Membership_Options() {
+	return (
+		<>
+			<h1>Become a member!</h1>
+			<h2>Regular membership - FREE</h2>
+
+			<ul>
+				<li>View your current tickets</li>
+				<li>Collect reward points and redeem for movie tickets</li>
+				<li>View your movie watch history</li>
+				<li>Book up to 8 seats for you and your friends</li>
+				<li>Cancel your tickets before showtime for a full refund</li>
+			</ul>
+
+			<h2>Premium membership - $15/year</h2>
+
+			<ul>
+				<li>All the perks of regular membership</li>
+				<li>Online service fee is waived for any booking</li>
+			</ul>
+		</>
+	);
+}
