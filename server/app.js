57f8cef36e177d797151b457bb89f68f63930e10
Sai kiran <saikiran.kammri@gmail.com>
2023-10-31 14:27:50 -0700

    membersModel.js

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const Movies = require('./models/moviesModel');
 const Theatres = require('./models/theatresModel');
 const Users = require('./models/usersModel');
+const Members = require('./models/membersModel');
 const app = express();
 
 app.use(express.static('public'));
@@ -11,5 +12,6 @@ app.use(express.urlencoded({extended:false}));
 Movies.find().then(movies => {console.log(movies);})
 Theatres.find().then(theatres => {console.log(theatres);})
 Users.find().then(users => {console.log(users);})
+Members.find().then(members => {console.log(members);})
 
 module.exports = app;
