f33b84622743a9e0f16e1f6b0b6fe683cf3d9cd2
SaiKiran <saikiran.kammari@gmail.com>
2023-10-31 17:06:34 -0700

    registrationEndpoint.js

--- server/server.js
+++ server/server.js
@@ -1,5 +1,43 @@
+const express = require('express');
 const mongoose = require('mongoose');
-const app = require('./app');
+const bodyParser = require('body-parser');
+const registrationRoutes = require('./endpointRoutes/registrationEndpoint');
+
+const app = express();
+
+const dburl = 'mongodb+srv://Mahee:Mahee123@cluster0.bndg37a.mongodb.net/movieDB?retryWrites=true&w=majority';
+
+async function connect() {
+    try {
+        await mongoose.connect(dburl, { useNewUrlParser: true, useUnifiedTopology: true });
+        console.log("Connected to MongoDB");
+
+        app.listen(3000, () => {
+            console.log("Server started on port 3000");
+        });
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+app.use(express.json()); // Using the built-in JSON parser from Express
+app.use(registrationRoutes);
+
+// Global error handler
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
+connect();
+
+
+/*const mongoose = require('mongoose');
+//const app = require('./app');
+const app = express();
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcryptjs');
 
 const dburl='mongodb+srv://Mahee:Mahee123@cluster0.bndg37a.mongodb.net/movieDB?retryWrites=true&w=majority'
 
@@ -17,3 +55,5 @@ connect();
 app.listen(3000, () =>{
     console.log("Server started on port 3000");
 });
+
+app.use(bodyParser.json());*/
