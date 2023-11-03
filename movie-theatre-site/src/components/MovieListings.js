3acadbbc5c1b28ad94411b20c19f166e8718564c
hponukumati <hponukumati@gmail.com>
2023-11-03 13:53:35 -0700

    movieListingChanges

--- movie-theatre-site/src/components/MovieListings.js
+++ movie-theatre-site/src/components/MovieListings.js
@@ -2,39 +2,34 @@ import React, { useState, useEffect } from "react";
 import Header from "./Header";
 
 const MovieListings = () => {
-  const [movies, setMovies] = useState([]);
+    const [movies, setMovies] = useState([]);
 
-  useEffect(() => {
-    fetch("/movies")
-      .then((response) => response.json())
-      .then((data) => setMovies(data))
-      .catch((error) =>
-        console.error("There was an error fetching movies:", error)
-      );
-  }, []);
+    useEffect(() => {
+        fetch("/movies")
+            .then((response) => response.json())
+            .then((data) => setMovies(data))
+            .catch((error) =>
+                console.error("There was an error fetching movies:", error)
+            );
+    }, []);
 
-  return (
-    <div className="mainListing">
-      <Header />
-      <p>This is for showing the list of the movies.</p>
-      <ul className="movies-list">
-        {movies.map((movie) => (
-          <li key={movie._id} className="movie-tile">
-            <img
-              src={movie.imageUrl}
-              alt={movie.movieName}
-              className="movie-image"
-            />
-            <div>
-              <h2>{movie.movieName}</h2>
-              <p>Status: {movie.status}</p>
-              <p>Description: {movie.description}</p>
-            </div>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+    return (
+        <div className="mainListing">
+            <Header />
+            <ul className="movies-list">
+                {movies.map((movie) => (
+                    <li key={movie._id} className="movie-tile">
+                        <img src={`${movie.img}`}alt={movie.movieName} className="movie-image"/>
+                        <div>
+                            <h2>{movie.movieName}</h2>
+                            <p>Status: {movie.status}</p>
+                            <p>Description: {movie.description}</p>
+                        </div>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 };
 
 export default MovieListings;
