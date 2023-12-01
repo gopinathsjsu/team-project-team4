92c90137d3c91ea6772e3de628a0f5818d87b8da
Sai kiran <saikiran.kammari@gmail.com>
2023-11-30 19:31:19 -0800

    Added delete movieHistory In delete api and addition of movieHistory in ticket creation api

--- server/endpointRoutes/ticketsEndpoint.js
+++ server/endpointRoutes/ticketsEndpoint.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const Tickets = require('../models/ticketsModel');
 const Showtimes = require('../models/showtimesModel');
 const Members = require('../models/membersModel');
+const Movies = require('../models/moviesModel');
 
 router.get('/tickets', async (request, response) => {
     try {
@@ -27,36 +28,49 @@ router.get('/tickets/:id', async (request, response) => {
 
 router.post('/tickets', async (request, response) => {
     try {
-        if (
-            !request.body.memberid || !request.body.seatsBooked || !request.body.showid  
-        ) {
+        if (!request.body.memberid || !request.body.seatsBooked || !request.body.showid) {
             return response.status(400).send({
-                message : 'Send all required fields'
+                message: 'Send all required fields'
             });
         }
         const newTicket = {
-            memberid : request.body.memberid,
-            seatsBooked : request.body.seatsBooked,
-            showid : request.body.showid
-        }
+            memberid: request.body.memberid,
+            seatsBooked: request.body.seatsBooked,
+            showid: request.body.showid
+        };
+
         const show = await Showtimes.findById(request.body.showid);
-        let n = request.body.seatsBooked.length;
-        for(let i = 0;i<n;i++)
-        {
-            show.seats_booked.push(request.body.seatsBooked[i]);
-        }
-        const member = await Members.findById(request.body.memberid);
-        member.rewards=n*show.price;
-        await Showtimes.findByIdAndUpdate(request.body.showid, show);
-        await Members.findByIdAndUpdate(request.body.memberid, member);
+        const movie = await Movies.findById(show.movieid); // Assuming show.movieid holds the movie's ID
+        const movieName = movie.movieName; // Assuming movie.movieName holds the movie's name
+        const currentDate = new Date();
+
+        // Add the booked seats to the show
+        const updatedShow = {
+            ...show.toObject(),
+            seats_booked: [...show.seats_booked, ...request.body.seatsBooked]
+        };
+
+        // Calculate the new rewards
+        const n = request.body.seatsBooked.length;
+        const newRewards = n * show.price;
+
+        // Update the showtimes and member in parallel
+        await Promise.all([
+            Showtimes.findByIdAndUpdate(request.body.showid, updatedShow),
+            Members.findByIdAndUpdate(request.body.memberid, {
+                $inc: { rewards: newRewards },
+                $push: { movieHistory: { date: currentDate, movieName: movieName } }
+            })
+        ]);
+
         const ticket = await Tickets.create(newTicket);
         return response.status(200).json(ticket);
-
     } catch (error) {
         console.log(error.message);
-        response.status(500).send({message : error.message});
+        response.status(500).send({ message: error.message });
     }
 });
+
 
 
 router.put('/tickets/:id', async (request, response) => {
@@ -83,30 +97,39 @@ router.put('/tickets/:id', async (request, response) => {
 
 router.delete('/tickets/:id', async (request, response) => {
     try {
-        const { id }  = request.params;
-        let ticket = await Tickets.findById(id);
+        const { id } = request.params;
+        const ticket = await Tickets.findById(id);
+        if (!ticket) {
+            return response.status(404).json({ message: 'Ticket not found' });
+        }
+
+        // Find the show and movie name
         const show = await Showtimes.findById(ticket.showid);
-        let ticketSeatsBooked = ticket.seatsBooked;
-        let arr = show.seats_booked;
-        for(let i = 0;i<ticketSeatsBooked.length;i++)
-        {
-            let index = arr.indexOf(ticketSeatsBooked[i]); 
-            if (index > -1) {
-            arr.splice(index, 1);
-            }
-        }
+        const movie = await Movies.findById(show.movieid);
+        const movieNameToDelete = movie.movieName;
+
+        // Update the showtimes by removing the booked seats
+        const updatedSeats = show.seats_booked.filter(seat => !ticket.seatsBooked.includes(seat));
+        await Showtimes.findByIdAndUpdate(ticket.showid, { seats_booked: updatedSeats });
+
+        // Update member's rewards and movie history
         const member = await Members.findById(ticket.memberid);
-        member.rewards-=ticketSeatsBooked.length*show.price;
-        await Showtimes.findByIdAndUpdate(ticket.showid, show);
-        await Members.findByIdAndUpdate(ticket.memberid, member);
-        ticket = await Tickets.findByIdAndDelete(id);
-        if(!ticket){
-            return response.status(404).json({ message : 'Ticket not found'});
+        if (member) {
+            member.rewards -= ticket.seatsBooked.length * show.price;
+            const updatedMovieHistory = member.movieHistory.filter((history, index, self) =>
+                !(history.movieName === movieNameToDelete && index === self.findIndex(h => h.movieName === movieNameToDelete))
+            );
+            member.movieHistory = updatedMovieHistory;
+            await member.save();
         }
-        return response.status(200).json({ message : 'Ticket deleted successfully'});
+
+        // Delete the ticket
+        await Tickets.findByIdAndDelete(id);
+
+        return response.status(200).json({ message: 'Ticket deleted successfully' });
     } catch (error) {
-        console.log(error.message);
-        response.status(500).send({message : error.message});
+        console.error('Error:', error.message);
+        response.status(500).send({ message: error.message });
     }
 });
 
